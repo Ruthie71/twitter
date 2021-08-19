8a4448e3e211c27761cde9cd13c21d67930fb2e6
waqarakram93-tech <84813788+waqarakram93-tech@users.noreply.github.com>
2021-08-19 17:51:59 +0200

    make it better

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-if (process.env.NODE_ENV !== 'production') {
-  await import('dotenv/config.js');
-}
+import 'dotenv/config.js';
+
 import express from 'express';
+import cors from 'cors'
 import postsRouter from './routes/postsRouter.js';
 import errorHandler from './middlewares/errorHandler.js';
+import './db/test.js'
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,9 +13,10 @@ if (process.env.NODE_ENV !== 'production') {
   const morgan = await import('morgan');
   app.use(morgan.default('dev'));
 }
-
+app.use(cors())
 app.use(express.json());
 app.use('/posts', postsRouter);
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import {
   createPost,
-  deletePost,
+
   getAllPosts,
   getSinglePost,
-  updatePost
+
 } from '../controllers/posts.js';
 
 const postsRouter = Router();
@@ -12,7 +12,7 @@ const postsRouter = Router();
 postsRouter.get('/', getAllPosts);
 postsRouter.get('/:id', getSinglePost);
 postsRouter.post('/', createPost);
-postsRouter.put('/:id', updatePost);
-postsRouter.delete('/:id', deletePost);
+// postsRouter.put('/:id', updatePost);
+// postsRouter.delete('/:id', deletePost);
 
 export default postsRouter;
